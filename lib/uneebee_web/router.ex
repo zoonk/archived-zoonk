5a121e279748d9f4bae338b93bd915d19a9f377f
Will Ceolin <wceolin@users.noreply.github.com>
2023-09-11 10:32:38 -0300

    Add CSP

--- lib/uneebee_web/router.ex
+++ lib/uneebee_web/router.ex
@@ -7,7 +7,7 @@ defmodule UneebeeWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {UneebeeWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
