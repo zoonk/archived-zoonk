43d5cbc7de6dbf58b7124fdc0c2f0236711244f1
Will Ceolin <wceolin@users.noreply.github.com>
2023-09-22 16:52:41 -0300

    Add school_user schema

---
+++ lib/organizations/school_user_schema.ex
@@ -0,0 +1,35 @@
+defmodule Uneebee.Organizations.SchoolUser do
+  @moduledoc """
+  Schema for the relationship between schools and users.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Uneebee.Accounts.User
+  alias Uneebee.Organizations.School
+
+  @type t :: %__MODULE__{}
+
+  schema "school_users" do
+    field :role, Ecto.Enum, values: [:manager, :teacher, :student], default: :student
+
+    field :approved?, :boolean, default: false
+    field :approved_at, :utc_datetime_usec
+    belongs_to :approved_by, User
+
+    belongs_to :school, School
+    belongs_to :user, User
+
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  @doc false
+  @spec changeset(Ecto.Schema.t(), map()) :: Ecto.Changeset.t()
+  def changeset(school_user, attrs \\ %{}) do
+    school_user
+    |> cast(attrs, [:approved?, :approved_at, :approved_by_id, :role, :school_id, :user_id])
+    |> validate_required([:role, :school_id, :user_id])
+    |> unique_constraint([:school_id, :user_id])
+  end
+end
---
+++ priv/repo/migrations/20230922194814_add_school_users_table.exs
@@ -0,0 +1,25 @@
+defmodule Uneebee.Repo.Migrations.AddSchoolUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:school_users) do
+      add :approved?, :boolean, default: false, null: false
+      add :approved_at, :utc_datetime_usec
+      add :approved_by_id, references(:users, on_delete: :nothing)
+      add :role, :string, default: "student", null: false
+      add :school_id, references(:schools, on_delete: :delete_all), null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime_usec)
+    end
+
+    create constraint(:school_users, :valid_role,
+             check: "role in ('manager', 'teacher', 'student')"
+           )
+
+    create index(:school_users, [:school_id])
+    create index(:school_users, [:user_id])
+    create index(:school_users, [:role, :school_id])
+    create unique_index(:school_users, [:school_id, :user_id])
+  end
+end
