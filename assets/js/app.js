f4b3e1b12a7d2f762fee4fd2050de0dda4561278
Will Ceolin <wceolin@users.noreply.github.com>
2023-09-30 09:46:57 -0300

    Fix hooks not being called

--- assets/js/app.js
+++ assets/js/app.js
@@ -81,6 +81,7 @@ let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content');
 let liveSocket = new LiveSocket('/live', Socket, {
+  hooks: Hooks,
   params: { _csrf_token: csrfToken },
 });
 
