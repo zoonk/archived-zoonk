35a0eb875e46b0167d9f7346992e1dcd5af1db2a
Will Ceolin <wceolin@users.noreply.github.com>
2023-10-10 19:42:31 -0300

    Mailer: Fix port being displayed

--- config/runtime.exs
+++ config/runtime.exs
@@ -90,7 +90,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "8080")
 
   config :uneebee, UneebeeWeb.Endpoint,
-    url: [host: host, port: port, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
