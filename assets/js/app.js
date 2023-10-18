9e89240149fee9f187aba7ec0d73c367488248a2
Will Ceolin <wceolin@users.noreply.github.com>
2023-10-18 18:39:23 -0300

    JS: Increase topbar delay

--- assets/js/app.js
+++ assets/js/app.js
@@ -133,7 +133,7 @@ let liveSocket = new LiveSocket('/live', Socket, {
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' });
-window.addEventListener('phx:page-loading-start', (_info) => topbar.show(600));
+window.addEventListener('phx:page-loading-start', (_info) => topbar.show(1000));
 window.addEventListener('phx:page-loading-stop', (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
