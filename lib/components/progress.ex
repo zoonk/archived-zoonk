7bc3e88064df93948d89b2b0c68184acf4c0259e
Will Ceolin <wceolin@users.noreply.github.com>
2023-10-23 18:23:21 -0300

    Progress: Set a maximum number of bars

--- lib/components/progress.ex
+++ lib/components/progress.ex
@@ -3,22 +3,32 @@ defmodule UneebeeWeb.Components.Progress do
   @moduledoc false
   use Phoenix.Component
 
+  @max 40
+
   attr :total, :integer, required: true
   attr :current, :integer, required: true
 
   def progress(assigns) do
     ~H"""
     <div class="border-gray-light2x flex w-full gap-1 rounded-2xl border p-2">
-      <% steps = Enum.map(1..@total, fn order -> order end) %>
+      <% total = calculate_total(@total) %>
+      <% current = calculate_current(@current, @total) %>
+      <% steps = Enum.map(1..total, fn order -> order end) %>
       <div
         :for={step <- steps}
         class={[
           "rounded-2xl w-full h-4 flex-1",
-          step <= @current && "bg-success-light",
-          step > @current && "bg-gray-light3x"
+          step <= current && "bg-success-light",
+          step > current && "bg-gray-light3x"
         ]}
       />
     </div>
     """
   end
+
+  defp calculate_total(total) when total > @max, do: @max
+  defp calculate_total(total), do: total
+
+  defp calculate_current(current, total) when total <= @max, do: current
+  defp calculate_current(current, total), do: (current * (@max / total)) |> round() |> max(1)
 end
