149c343584482a46d025c648cb86692c5c39eb14
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-03 22:25:04 -0300

    update prettier options

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,21 +1,21 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require('tailwindcss/plugin');
-const colors = require('tailwindcss/colors');
-const fs = require('fs');
-const path = require('path');
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-  content: ['./js/**/*.js', '../lib/*.ex', '../lib/**/*.*ex'],
+  content: ["./js/**/*.js", "../lib/*.ex", "../lib/**/*.*ex"],
   theme: {
     extend: {
       screens: {
-        '3xl': '1800px',
-        '4xl': '2100px',
-        '5xl': '2400px',
-        '6xl': '2700px',
-        '7xl': '3000px',
+        "3xl": "1800px",
+        "4xl": "2100px",
+        "5xl": "2400px",
+        "6xl": "2700px",
+        "7xl": "3000px",
       },
       colors: {
         success: {
@@ -56,79 +56,58 @@ module.exports = {
         },
       },
       boxShadow: {
-        'b-gray-light': `3px 3px 0px 0px ${colors.slate[100]}`,
-        'b-gray-light-pressed': `1px 1px 0px 0px ${colors.slate[100]}`,
-        'b-gray': `3px 3px 0px 0px ${colors.slate[300]}`,
-        'b-gray-pressed': `1px 1px 0px 0px ${colors.slate[300]}`,
-        'b-cyan-400': `3px 3px 0px 0px ${colors.cyan[100]}`,
-        'b-cyan-400-pressed': `1px 1px 0px 0px ${colors.cyan[100]}`,
-        'b-cyan': `3px 3px 0px 0px ${colors.cyan[300]}`,
-        'b-cyan-pressed': `1px 1px 0px 0px ${colors.cyan[300]}`,
-        'b-success-light': `3px 3px 0px 0px ${colors.teal[100]}`,
-        'b-success-light-pressed': `1px 1px 0px 0px ${colors.teal[100]}`,
-        'b-pink-400': `3px 3px 0px 0px ${colors.pink[100]}`,
-        'b-pink-400-pressed': `1px 1px 0px 0px ${colors.pink[100]}`,
-        'b-pink': `3px 3px 0px 0px ${colors.pink[300]}`,
-        'b-pink-pressed': `1px 1px 0px 0px ${colors.pink[300]}`,
-        'b-pink-700': `3px 3px 0px 0px ${colors.pink[700]}`,
-        'b-pink-700-pressed': `1px 1px 0px 0px ${colors.pink[700]}`,
+        "b-gray-light": `3px 3px 0px 0px ${colors.slate[100]}`,
+        "b-gray-light-pressed": `1px 1px 0px 0px ${colors.slate[100]}`,
+        "b-gray": `3px 3px 0px 0px ${colors.slate[300]}`,
+        "b-gray-pressed": `1px 1px 0px 0px ${colors.slate[300]}`,
+        "b-cyan-400": `3px 3px 0px 0px ${colors.cyan[100]}`,
+        "b-cyan-400-pressed": `1px 1px 0px 0px ${colors.cyan[100]}`,
+        "b-cyan": `3px 3px 0px 0px ${colors.cyan[300]}`,
+        "b-cyan-pressed": `1px 1px 0px 0px ${colors.cyan[300]}`,
+        "b-success-light": `3px 3px 0px 0px ${colors.teal[100]}`,
+        "b-success-light-pressed": `1px 1px 0px 0px ${colors.teal[100]}`,
+        "b-pink-400": `3px 3px 0px 0px ${colors.pink[100]}`,
+        "b-pink-400-pressed": `1px 1px 0px 0px ${colors.pink[100]}`,
+        "b-pink": `3px 3px 0px 0px ${colors.pink[300]}`,
+        "b-pink-pressed": `1px 1px 0px 0px ${colors.pink[300]}`,
+        "b-pink-700": `3px 3px 0px 0px ${colors.pink[700]}`,
+        "b-pink-700-pressed": `1px 1px 0px 0px ${colors.pink[700]}`,
       },
       animation: {
-        'slide-up': 'slide-up 0.5s ease-out forwards',
+        "slide-up": "slide-up 0.5s ease-out forwards",
       },
       keyframes: {
-        'slide-up': {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+        "slide-up": {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/container-queries'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries"),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
     //
     //     <div class="phx-click-loading:animate-ping">
     //
-    plugin(({ addVariant }) =>
-      addVariant('phx-no-feedback', ['.phx-no-feedback&', '.phx-no-feedback &'])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant('phx-click-loading', [
-        '.phx-click-loading&',
-        '.phx-click-loading &',
-      ])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant('phx-submit-loading', [
-        '.phx-submit-loading&',
-        '.phx-submit-loading &',
-      ])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant('phx-change-loading', [
-        '.phx-change-loading&',
-        '.phx-change-loading &',
-      ])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant('drag-item', ['.drag-item&', '.drag-item &'])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant('drag-ghost', ['.drag-ghost&', '.drag-ghost &'])
-    ),
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+    plugin(({ addVariant }) => addVariant("drag-item", [".drag-item&", ".drag-item &"])),
+    plugin(({ addVariant }) => addVariant("drag-ghost", [".drag-ghost&", ".drag-ghost &"])),
 
     // Embeds Tabler Icons (https://tabler-icons.io/) into your app.css bundle
     plugin(function ({ matchComponents, theme }) {
-      let iconsDir = path.join(__dirname, './vendor/tabler/icons');
-      let values = {};
-      let icons = [['', '']];
+      const iconsDir = path.join(__dirname, "./vendor/tabler/icons");
+      const values = {};
+      const icons = [["", ""]];
 
       icons.forEach(([suffix, dir]) => {
         fs.readdirSync(path.join(iconsDir, dir)).map((file) => {
-          let name = path.basename(file, '.svg') + suffix;
+          const name = path.basename(file, ".svg") + suffix;
           values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
         });
       });
@@ -136,19 +115,20 @@ module.exports = {
       matchComponents(
         {
           tabler: ({ name, fullPath }) => {
-            let content = fs
+            const content = fs
               .readFileSync(fullPath)
               .toString()
-              .replace(/\r?\n|\r/g, '');
+              .replace(/\r?\n|\r/g, "");
+
             return {
               [`--tabler-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-              '-webkit-mask': `var(--tabler-${name})`,
+              "-webkit-mask": `var(--tabler-${name})`,
               mask: `var(--tabler-${name})`,
-              'background-color': 'currentColor',
-              'vertical-align': 'middle',
-              display: 'inline-block',
-              width: theme('spacing.5'),
-              height: theme('spacing.5'),
+              "background-color": "currentColor",
+              "vertical-align": "middle",
+              display: "inline-block",
+              width: theme("spacing.5"),
+              height: theme("spacing.5"),
             };
           },
         },
