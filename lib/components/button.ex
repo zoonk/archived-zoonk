b036f4e87ec1423621960763ea3987b02ef0c9ed
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-03 23:33:19 -0300

    remove shadow from buttons

--- lib/components/button.ex
+++ lib/components/button.ex
@@ -35,12 +35,12 @@ defmodule UneebeeWeb.Components.Button do
         "rounded-lg px-3 py-2 focus:outline-offset-2 phx-submit-loading:opacity-75",
         "text-sm font-semibold leading-6",
         "disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400",
-        @color == :black && "shadow-b-gray bg-gray-700 text-white hover:bg-gray-900 focus:outline-gray-700 active:shadow-b-gray-pressed",
-        @color == :alert && "shadow-b-pink-700 bg-pink-500 text-white hover:bg-pink-700 focus:outline-pink-500 active:shadow-b-pink-700-pressed",
-        @color == :success && "shadow-b-teal-700 bg-teal-500 text-white hover:bg-teal-700 focus:outline-teal-500 active:shadow-b-teal-700-pressed",
-        @color == :alert_light && "shadow-b-pink-400 bg-pink-50 text-pink-900 hover:bg-pink-200 focus:outline-pink-50 active:shadow-b-pink-400-pressed",
-        @color == :info_light && "shadow-b-cyan-400 bg-cyan-50 text-cyan-900 hover:bg-cyan-200 focus:outline-cyan-50 active:shadow-b-cyan-400-pressed",
-        @color == :success_light && "shadow-b-teal-400 bg-teal-50 text-teal-900 hover:bg-teal-200 focus:outline-teal-50 active:shadow-b-teal-400-pressed",
+        @color == :black && "bg-gray-700 text-white hover:bg-gray-900 focus:outline-gray-700",
+        @color == :alert && "bg-pink-500 text-white hover:bg-pink-700 focus:outline-pink-500",
+        @color == :success && "bg-teal-500 text-white hover:bg-teal-700 focus:outline-teal-500",
+        @color == :alert_light && "bg-pink-50 text-pink-900 hover:bg-pink-200 focus:outline-pink-50",
+        @color == :info_light && "bg-cyan-50 text-cyan-900 hover:bg-cyan-200 focus:outline-cyan-50",
+        @color == :success_light && "bg-teal-50 text-teal-900 hover:bg-teal-200 focus:outline-teal-50",
         @class
       ]}
       {@rest}
@@ -72,7 +72,7 @@ defmodule UneebeeWeb.Components.Button do
         "rounded-lg px-3 py-2 focus:outline-offset-2 phx-submit-loading:opacity-75",
         "text-sm font-semibold leading-6",
         "disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400",
-        "shadow-b-pink-400 bg-pink-50 text-pink-900 hover:bg-pink-200 focus:outline-pink-50 active:shadow-b-pink-400-pressed",
+        "bg-pink-50 text-pink-900 hover:bg-pink-200 focus:outline-pink-50",
         @size == :sm && "h-6 w-6",
         @size == :md && "h-8 w-8",
         @size == :lg && "h-10 w-10"
