a555b7be56f20dd471a7857cf5ae543e8516cf71
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-05 15:49:59 -0300

    course select redirects to the proper page

--- lib/layouts/components/course_select.ex
+++ lib/layouts/components/course_select.ex
@@ -26,11 +26,17 @@ defmodule UneebeeWeb.Components.Layouts.CourseSelect do
 
   @impl Phoenix.LiveComponent
   def handle_event("select-course", %{"course" => course_slug}, socket) do
-    {:noreply, push_navigate(socket, to: course_link(course_slug))}
+    {:noreply, push_navigate(socket, to: course_link(course_slug, socket.assigns.page))}
   end
 
-  defp course_link("new-course"), do: ~p"/dashboard/courses/new"
-  defp course_link(slug), do: ~p"/dashboard/c/#{slug}"
+  defp course_link("new-course", _page), do: ~p"/dashboard/courses/new"
+  defp course_link(slug, :dashboard_courseuserlist_student), do: ~p"/dashboard/c/#{slug}/students"
+  defp course_link(slug, :dashboard_coursestudentview), do: ~p"/dashboard/c/#{slug}/students"
+  defp course_link(slug, :dashboard_courseuserlist_teacher), do: ~p"/dashboard/c/#{slug}/teachers"
+  defp course_link(slug, :dashboard_courseedit_settings), do: ~p"/dashboard/c/#{slug}/edit/settings"
+  defp course_link(slug, :dashboard_courseedit_cover), do: ~p"/dashboard/c/#{slug}/edit/cover"
+  defp course_link(slug, :dashboard_courseedit_delete), do: ~p"/dashboard/c/#{slug}/edit/delete"
+  defp course_link(slug, _page), do: ~p"/dashboard/c/#{slug}"
 
   defp list_courses(school_id, _user, :manager), do: Content.list_courses_by_school(school_id)
   defp list_courses(_school, user_id, :teacher), do: Content.list_courses_by_user(user_id, :teacher)
