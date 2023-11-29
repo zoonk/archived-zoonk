ac76e898cf9b3236e0aecbe5f5fae788282a3c26
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-29 04:07:47 -0300

    list schools

--- test/dashboard/school_list_test.exs
+++ test/dashboard/school_list_test.exs
@@ -1,6 +1,9 @@
 defmodule UneebeeWeb.DashboardSchoolListTest do
   use UneebeeWeb.ConnCase, async: true
 
+  import Phoenix.LiveViewTest
+  import Uneebee.Fixtures.Organizations
+
   describe "/dashboard/schools (non-authenticated users)" do
     setup :set_school
 
@@ -17,6 +20,25 @@ defmodule UneebeeWeb.DashboardSchoolListTest do
 
     test "returns 403", %{conn: conn} do
       assert_error_sent(403, fn -> get(conn, ~p"/dashboard/schools") end)
+    end
+  end
+
+  describe "/dashboard/schools (managers)" do
+    setup do
+      app_setup(%{conn: build_conn()}, school_user: :manager)
+    end
+
+    test "lists schools", %{conn: conn, school: school} do
+      schools = Enum.map(1..3, fn idx -> school_fixture(%{school_id: school.id, public?: idx == 1, name: "School #{idx}!"}) end)
+
+      {:ok, lv, _html} = live(conn, "/dashboard/schools")
+
+      Enum.each(schools, fn school ->
+        status = if school.public?, do: "Public", else: "Private"
+
+        assert has_element?(lv, "h3", school.name)
+        assert has_element?(lv, "#school-#{school.id} span", status)
+      end)
     end
   end
 end
