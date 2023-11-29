823d38bc6cba994c0430774d674bd7d9664a885f
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-29 14:03:37 -0300

    Dashboard: Make header sticky (fixes #70)

--- lib/dashboard/components/user_list_header.ex
+++ lib/dashboard/components/user_list_header.ex
@@ -3,9 +3,19 @@ defmodule UneebeeWeb.Components.Dashboard.UserListHeader do
   @moduledoc false
   use UneebeeWeb, :html
 
+  attr :title, :string, required: true
+  attr :course?, :boolean, default: false
+  attr :count, :integer, required: true
+  attr :add_user_label, :string, required: true
+  attr :on_add_user, :any, required: true
+
   def user_list_header(assigns) do
     ~H"""
-    <div class="flex flex-wrap items-center gap-2 bg-gray-50 p-4 sm:flex-nowrap sm:px-6 lg:px-8">
+    <div class={[
+      "sticky z-40 flex flex-wrap items-center gap-2 bg-gray-50 p-4 sm:flex-nowrap sm:px-6 lg:px-8",
+      @course? && "top-[132px] sm:top-[128px]",
+      !@course? && "top-[57px]"
+    ]}>
       <h1 class="text-base font-semibold leading-7 text-gray-900"><%= @title %></h1>
       <.badge color={:info}><%= @count %></.badge>
       <.button phx-click={@on_add_user} icon="tabler-user-plus" class="ml-auto"><%= @add_user_label %></.button>
