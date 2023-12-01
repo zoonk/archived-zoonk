a54da3c8b7dd435504e0c567a782f39815ad1427
Will Ceolin <wceolin@users.noreply.github.com>
2023-12-01 13:41:31 -0300

    Modal: Fix height (fixes #84)

--- lib/components/modal.ex
+++ lib/components/modal.ex
@@ -40,14 +40,14 @@ defmodule UneebeeWeb.Components.Modal do
     <div id={@id} phx-mounted={@show && show_modal(@id)} phx-remove={hide_modal(@id)} data-cancel={JS.exec(@on_cancel, "phx-remove")} class="relative z-50 hidden">
       <div id={"#{@id}-bg"} class="bg-gray-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
       <div class="fixed inset-0 overflow-y-auto" aria-labelledby={"#{@id}-title"} aria-describedby={"#{@id}-description"} role="dialog" aria-modal="true" tabindex="0">
-        <div class="flex min-h-full items-center justify-center">
+        <div class="min-h-dvh flex items-center justify-center">
           <div class="w-full max-w-3xl sm:p-6 lg:py-8">
             <.focus_wrap
               id={"#{@id}-container"}
               phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
               phx-key="escape"
               phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-gray-700/10 ring-gray-700/10 relative hidden min-h-dvh bg-white p-8 shadow-lg ring-1 transition lg:h-auto lg:rounded-2xl"
+              class="shadow-gray-700/10 ring-gray-700/10 relative hidden bg-white p-8 shadow-lg ring-1 transition lg:h-auto lg:rounded-2xl"
             >
               <div class="absolute top-6 right-5">
                 <button phx-click={JS.exec("data-cancel", to: "##{@id}")} type="button" class="-m-3 flex-none p-3 opacity-20 hover:opacity-40" aria-label={gettext("close")}>
