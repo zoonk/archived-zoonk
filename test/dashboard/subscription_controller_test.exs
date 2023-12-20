d8120fcd93b9a394fb567c85723c52475fc949ba
Will Ceolin <wceolin@users.noreply.github.com>
2023-12-20 00:03:04 -0300

    update billing tests

--- test/dashboard/subscription_controller_test.exs
+++ test/dashboard/subscription_controller_test.exs
@@ -1,41 +1,58 @@
 defmodule UneebeeWeb.SubscriptionControllerTest do
   use UneebeeWeb.ConnCase, async: true
 
-  describe "GET /dashboard/subscribe (non-authenticated users)" do
+  import Uneebee.Fixtures.Billing
+  import Uneebee.Fixtures.Organizations
+
+  alias Uneebee.Billing
+
+  describe "GET /dashboard/billing/:price_id (non-authenticated users)" do
     setup :set_school
 
     test "redirects to login page", %{conn: conn} do
-      conn = get(conn, ~p"/dashboard/subscribe")
+      conn = get(conn, ~p"/dashboard/billing/free/flexible/usd/pri_123")
       assert redirected_to(conn) == ~p"/users/login"
     end
   end
 
-  describe "GET /dashboard/subscribe (students)" do
+  describe "GET /dashboard/billing/:price_id (students)" do
     setup :app_setup
 
     test "returns 403", %{conn: conn} do
-      assert_error_sent(403, fn -> get(conn, ~p"/dashboard/subscribe") end)
+      assert_error_sent(403, fn -> get(conn, ~p"/dashboard/billing/free/flexible/usd/pri_123") end)
     end
   end
 
-  describe "GET /dashboard/subscribe (teachers)" do
+  describe "GET /dashboard/billing/:price_id (teachers)" do
     setup do
       app_setup(%{conn: build_conn()}, school_user: :teacher)
     end
 
     test "returns 403", %{conn: conn} do
-      assert_error_sent(403, fn -> get(conn, ~p"/dashboard/subscribe") end)
+      assert_error_sent(403, fn -> get(conn, ~p"/dashboard/billing/free/flexible/usd/pri_123") end)
     end
   end
 
-  describe "GET /dashboard/subscribe (managers)" do
+  describe "GET /dashboard/billing/:price_id (managers)" do
     setup do
       app_setup(%{conn: build_conn()}, school_user: :manager)
     end
 
-    test "redirects to the checkout page", %{conn: conn} do
-      conn = get(conn, ~p"/dashboard/subscribe")
+    test "redirects to the checkout page when upgrading", %{conn: conn} do
+      conn = get(conn, ~p"/dashboard/billing/free/flexible/usd/pri_123")
       assert redirected_to(conn) =~ "/pay/c/cs_test_"
+    end
+
+    test "deletes the subscription when downgrading", %{conn: conn, school: school, user: user} do
+      child_school = school_fixture(%{school_id: school.id})
+      school_user_fixture(%{school: child_school, user: user, role: :manager})
+
+      host = "#{child_school.slug}.#{school.custom_domain}"
+      subscription_fixture(%{school_id: child_school.id})
+
+      conn = conn |> Map.put(:host, host) |> get(~p"/dashboard/billing/flexible/free/usd/pri_123")
+      assert redirected_to(conn) == ~p"/dashboard/billing"
+      assert Billing.get_subscription_by_school_id(child_school.id) == nil
     end
   end
 end
