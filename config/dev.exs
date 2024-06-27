d75e0216d21b8f0104a9da30bfdb7b8fd123cfe3
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-27 19:59:00 -0300

    Dev: Improve LiveView debugging

--- config/dev.exs
+++ config/dev.exs
@@ -60,5 +60,11 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
+config :phoenix_live_view,
+  # Include HEEx debug annotations as HTML comments in rendered markup
+  debug_heex_annotations: true,
+  # Enable helpful, but potentially expensive runtime checks
+  enable_expensive_runtime_checks: true
+
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
