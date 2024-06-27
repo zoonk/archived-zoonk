f2df592dac37669570533d610c86dd49f6f00ec3
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-27 20:06:27 -0300

    Config: Remove unnecessary host in prod

--- config/prod.exs
+++ config/prod.exs
@@ -9,9 +9,7 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :zoonk, ZoonkWeb.Endpoint,
-  url: [host: System.get_env("PHX_HOST"), port: System.get_env("PORT")],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+config :zoonk, ZoonkWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :zoonk, :csp, connect_src: System.get_env("CSP_CONNECT_SRC")
 config :zoonk, :cdn, url: System.get_env("CDN_URL")
