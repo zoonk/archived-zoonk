08309140f1bcfacd4f26f669920bdcc95a0a6f61
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-27 16:39:20 -0300

    Config: Bump tailwind to 3.4.4

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.3.2",
+  version: "3.4.4",
   default: [
     args: ~w(
       --config=tailwind.config.js
