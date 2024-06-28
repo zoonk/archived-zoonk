bced4750ac03c6e5bc30b720d6c2c1eb257a5d84
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-28 18:45:31 -0300

    Test: Enable runtime checks

--- config/test.exs
+++ config/test.exs
@@ -35,6 +35,10 @@ config :logger, level: :warning
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
+
 # Stripe test keys
 config :stripity_stripe,
   api_key: "sk_test_thisisaboguskey",
