d04709b1ffc2a8c12049850f451a9e0689203055
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-28 19:14:54 -0300

    Endpoint: Enable longpoll

--- lib/endpoint.ex
+++ lib/endpoint.ex
@@ -12,7 +12,9 @@ defmodule ZoonkWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [:uri, session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [connect_info: [:uri, session: @session_options]],
+    longpoll: [connect_info: [:uri, session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
