2799abc5b3ba702adb712a7f2d4a2be2df4943b9
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-28 18:41:20 -0300

    Test: Update pool_size

--- config/test.exs
+++ config/test.exs
@@ -14,7 +14,7 @@ config :zoonk, Zoonk.Repo,
   hostname: "localhost",
   database: "zoonk_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: System.schedulers_online() * 2
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
