c43589cfdd7cc900c88665a3cdff104aed87f01b
Will Ceolin <wceolin@users.noreply.github.com>
2024-07-21 16:40:36 -0300

    move presign logic to the storage module

--- lib/components/upload.ex
+++ lib/components/upload.ex
@@ -93,17 +93,7 @@ defmodule ZoonkWeb.Components.Upload do
   end
 
   defp presign_upload(entry, socket) do
-    config = ExAws.Config.new(:s3)
-    bucket = StorageAPI.get_bucket()
-    timestamp = DateTime.to_unix(DateTime.utc_now())
-    key = "#{timestamp}_#{entry.client_name}"
-
-    {:ok, url} =
-      ExAws.S3.presigned_url(config, :put, bucket, key,
-        expires_in: 3600,
-        query_params: [{"Content-Type", entry.client_type}]
-      )
-
+    {url, key} = StorageAPI.presigned_url(entry)
     {:ok, %{uploader: "S3", key: key, url: url}, assign(socket, :uploaded_file_key, key)}
   end
 
--- lib/storage/storage_api.ex
+++ lib/storage/storage_api.ex
@@ -3,8 +3,10 @@ defmodule Zoonk.Storage.StorageAPI do
   Provides a way to upload files to the storage service.
   """
   alias ExAws.S3
+  alias Phoenix.LiveView.UploadEntry
 
   @callback delete(String.t()) :: {:ok, term()} | {:error, term()}
+  @callback presigned_url(UploadEntry.t()) :: {String.t(), String.t()}
 
   @doc """
   Deletes a file from the storage service.
@@ -19,6 +21,17 @@ defmodule Zoonk.Storage.StorageAPI do
   """
   @spec delete(String.t()) :: {:ok, term()} | {:error, term()}
   def delete(key), do: impl().delete(key)
+
+  @doc """
+  Generates a presigned URL for a file upload.
+
+  ## Examples
+
+      iex> StorageAPI.presigned_url(%UploadEntry{})
+      "https://..."
+  """
+  @spec presigned_url(UploadEntry.t()) :: {String.t(), String.t()}
+  def presigned_url(entry), do: impl().presigned_url(entry)
 
   @doc """
   Gets the URL of a file from the storage service.
@@ -71,6 +84,7 @@ end
 defmodule Zoonk.ExternalStorageAPI do
   @moduledoc false
   alias ExAws.S3
+  alias Phoenix.LiveView.UploadEntry
   alias Zoonk.Storage.StorageAPI
 
   @spec delete(String.t()) :: {:ok, term()} | {:error, term()}
@@ -78,5 +92,21 @@ defmodule Zoonk.ExternalStorageAPI do
     StorageAPI.get_bucket()
     |> S3.delete_object(key)
     |> ExAws.request()
+  end
+
+  @spec presigned_url(UploadEntry.t()) :: {String.t(), String.t()}
+  def presigned_url(%UploadEntry{client_name: client_name, client_type: client_type}) do
+    config = ExAws.Config.new(:s3)
+    bucket = StorageAPI.get_bucket()
+    timestamp = DateTime.to_unix(DateTime.utc_now())
+    key = "#{timestamp}_#{client_name}"
+
+    {:ok, url} =
+      ExAws.S3.presigned_url(config, :put, bucket, key,
+        expires_in: 3600,
+        query_params: [{"Content-Type", client_type}]
+      )
+
+    {url, key}
   end
 end
