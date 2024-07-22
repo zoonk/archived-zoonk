b58da2ac9d76f8558b9135778cbd0fa22af4007c
Will Ceolin <wceolin@users.noreply.github.com>
2024-07-22 20:56:54 -0300

    add mox allow

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,7 @@ Ecto.Adapters.SQL.Sandbox.mode(Zoonk.Repo, :manual)
 # Mock the external storage API for tests
 Mox.defmock(Zoonk.Storage.StorageAPIMock, for: Zoonk.Storage.StorageAPIBehaviour)
 Application.put_env(:zoonk, :storage_api, Zoonk.Storage.StorageAPIMock)
+
+Mox.allow(Zoonk.Storage.StorageAPIMock, self(), fn ->
+  GenServer.whereis(Zoonk.FLAME.ImageOptimization)
+end)
